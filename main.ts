551c94adad58c83bab1e46a06162ce53dd3504f9
David Poole <14233215+davidlpoole@users.noreply.github.com>
2024-01-16 17:08:36 +1300

    Use zenstack enhanced prisma client

--- main.ts
+++ main.ts
@@ -1,11 +1,26 @@
 import { PrismaClient } from '@prisma/client'
+import { enhance } from '@zenstackhq/runtime'
 import { ZenStackMiddleware } from '@zenstackhq/server/express'
-import express from 'express'
+import express, { Request } from 'express'
 
 const app = express()
 app.use(express.json())
 
 const prisma = new PrismaClient()
-app.use('/api/rpc', ZenStackMiddleware({ getPrisma: () => prisma }))
+
+function getUser(req: Request) {
+  if (req.headers['x-user-id']) {
+    return { id: parseInt(req.headers['x-user-id'] as string) }
+  } else {
+    return undefined
+  }
+}
+
+app.use(
+  '/api/rpc',
+  ZenStackMiddleware({
+    getPrisma: (req) => enhance(prisma, { user: getUser(req) }),
+  })
+)
 
 app.listen(3000, () => console.log('🚀 Server ready at: http://localhost:3000'))
