97d88ff24728b137db42cc186f5ec9da6d80f893
David Poole <14233215+davidlpoole@users.noreply.github.com>
2024-01-16 17:18:26 +1300

    Add OpenAPI/SwaggerUI

--- main.ts
+++ main.ts
@@ -3,6 +3,7 @@ import { enhance } from '@zenstackhq/runtime'
 import { ZenStackMiddleware } from '@zenstackhq/server/express'
 import express, { Request } from 'express'
 import RESTHandler from '@zenstackhq/server/api/rest'
+import swaggerUI from 'swagger-ui-express'
 
 const app = express()
 app.use(express.json())
@@ -30,6 +31,12 @@ app.use(
     handler: RESTHandler({ endpoint: 'http://localhost:3000/api/rest' }),
     getPrisma: (req) => enhance(prisma, { user: getUser(req) }),
   })
+)
+
+app.use(
+  '/api/docs',
+  swaggerUI.serve,
+  swaggerUI.setup(require('./todo-api.json'))
 )
 
 app.listen(3000, () => console.log('🚀 Server ready at: http://localhost:3000'))
